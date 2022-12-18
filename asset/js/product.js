8f1e1193cbad43e63db5d25e312df5f62425546c
ylen000 <wendy20020213@gmail.com>
2022-12-18 11:39:57 +0800

    整合sherry和ylen檔案

---
+++ asset/js/product.js
@@ -0,0 +1,145 @@
+//01
+function myFunction01() {
+    x = documeAnt.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_bestwishes.png'>";    // 改变内容，注意雙引號內不可再有雙引號，故改用單引號
+}
+//02
+function myFunction02() {
+    x = documeAnt.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_love.png'>";
+}
+//03
+function myFunction03() {
+    x = documeAnt.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_foryou.png'>";
+}
+//04
+function myFunction04() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_miss.png'>";
+}
+function myFunction041() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_miss1.png'>";
+}
+//05
+function myFunction05() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_beloved.png'>";
+}
+function myFunction051() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_beloved1.png'>";
+}
+//06
+function myFunction06() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_admire.png'>";
+}
+function myFunction061() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_admire1.png'>";
+}
+//07
+function myFunction07() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_stubborn.png'>";
+}
+function myFunction071() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_stubborn1.png'>";
+}
+//08
+function myFunction08() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_queen.png'>";
+}
+function myFunction081() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_queen1.png'>";
+}
+//09
+function myFunction09() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_red.png'>";
+}
+function myFunction091() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_red1.png'>";
+}
+//10
+function myFunction10() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_gold.png'>";
+}
+function myFunction101() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_gold1.png'>";
+}
+//11
+function myFunction11() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_silver.png'>";
+}
+function myFunction111() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_silver1.png'>";
+}
+//12
+function myFunction12() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_milktea.png'>";
+}
+function myFunction121() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_milktea1.png'>";
+}
+//13
+function myFunction13() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_sunset.png'>";
+}
+function myFunction131() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_sunset1.png'>";
+}
+//14
+function myFunction14() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_forest.png'>";
+}
+function myFunction141() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_forest1.png'>";
+}
+//15
+function myFunction15() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_blue.png'>";
+}
+function myFunction151() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_blue1.png'>";
+}
+//16
+function myFunction16() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_pink.png'>";
+}
+function myFunction161() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_wax_pink1.png'>";
+}
+//17
+function myFunction17() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_tool.png'>";
+}
+function myFunction171() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_tool1.png'>";
+}
+function myFunction172() {
+    x = document.getElementById("big_photo");
+    x.innerHTML = "<img src='../assets/imsges/produce_tool2.png'>";
+}
