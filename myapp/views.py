ea8391d7c2cea5a7d850ec6073c24b395c7c6c74
ylen000 <wendy20020213@gmail.com>
2022-12-29 15:27:20 +0800

    登入後端完成

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from myapp.models import userdata
 from django.http import Http404
 from django.contrib import auth
 
+
 # Create your views here.   
 
 def signin(request):
@@ -12,15 +13,30 @@ def signin(request):
 def signup(request):
     return render(request, 'signup.html')
 def login(request):
-     uName = request.POST.get('uName') 
-     uPass = request.POST.get('uPass') 
-     user=userdata.objects.filter(PHONE=uName, PASSWORD=uPass)
-     if user is not None:
+    uName = request.POST.get('uName') 
+    uPass = request.POST.get('psw') 
+    try:                
+        user=userdata.objects.get(PHONE=uName)
+                
+        if uPass==user.PASSWORD:          
+                #userlogin = request.session.get(uName)  
+            username=str(user.NAME)
+            request.session["username"]=username
+            return render(request, 'welcome.html',locals())     
+        else:
+            wrong="帳號或密碼錯誤!!"
+            return render(request,'passwordwrong.html',locals())
+    except userdata.DoesNotExist:
+            wrong="尚未註冊"
+            return render(request,'passwordwrong.html',locals())
+        
+def home(request):
+    username=request.session.get('username')
+    return render(request,'index.html',locals())
+         
+    
+    
             
-            return redirect("您好")
-     else:
-            return redirect('/login/')
-
     
 # Create your views here.
 
