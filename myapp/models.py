7ca72f5067e05e6fcdec5123b5be47c2e5004d83
OSASrrrrr <ggipe7309c@gmail.com>
2022-12-31 00:17:20 +0800

    lu

--- myapp/models.py
+++ myapp/models.py
@@ -9,14 +9,12 @@ class userdata(models.Model):
         NICKNAME = models.CharField('暱稱',max_length=20,blank=True)
         POINT = models.IntegerField('點數',blank=True)
         IDCARD = models.CharField('身分證字號',max_length=10,blank=True)
-<<<<<<< HEAD
 
 
 
 class QA(models.Model):
         QUESTIONS = models.CharField('問題回報',max_length=500,blank = False,null=False)
-=======
+
 class memberlevel(models.Model):
         IMAGE = models.ImageField(upload_to="../static/images",blank=False, null=False)
         IMAGE_NUMBER = models.CharField('等級',max_length=32)
->>>>>>> 758235adf62ffa7c20e6df7cfeae145dd7cb46e3
